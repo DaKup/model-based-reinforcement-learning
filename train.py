6a198b6b39bd71cbf4b94b8f0fab3602d4ceb460
Daniel <daniel.kup@student.tugraz.at>
2020-07-28 02:08:45 +0200

    moved environment to gym

---
+++ train.py
@@ -0,0 +1,34 @@
+import argparse
+import gym
+from src.env import ToyEnv
+import numpy as np
+
+def main():
+
+    env = ToyEnv()
+    env.reset()
+
+    total_reward = 0
+    episode = []
+    for _ in range(10000):
+        env.render('human')
+        action = env.action_space.sample()
+        state = env.state_space.sample()
+
+        # manual policy:
+        agent_position = state[0]
+        target_position = state[2][0]
+        action = np.ones_like(action)
+        action[np.where(agent_position > target_position)] = 0
+        action[np.where(agent_position < target_position)] = 2
+
+        (reward, state) = env.step(action)
+        episode.append((state, action, reward))
+        total_reward += reward
+    env.close()
+
+    print(total_reward)
+
+
+if __name__ == "__main__":
+    main()
