1153a8b13480b491b1be1d8517df187810ffc39e
Daniel <daniel.kup@student.tugraz.at>
2021-03-28 00:57:02 +0100

    added transition_net, reward_net

--- collectdatatest.py
+++ collectdatatest.py
@@ -38,11 +38,32 @@ def main():
     observations_actions = tf.concat([observations[:-1], actions], axis=1)
     dataset1 = (observations_actions, rewards)
     dataset2 = (observations_actions, observations[1:])
-    pass
+
+
+    transition_net = keras.Sequential([
+        keras.layers.Dense(32, input_shape=observations_actions.shape[1:]),
+        keras.layers.Activation('relu'),
+        keras.layers.Dense(32),
+        keras.layers.Activation('relu'),
+        keras.layers.Dense(observations.shape[1])
+    ])
+    transition_net.compile(optimizer='adam', loss='mse')
+    transition_net.fit(observations_actions,  observations[1:], epochs=15, batch_size=10)
+    
+    reward_net = keras.Sequential([
+        keras.layers.Dense(32, input_shape=observations_actions.shape[1:]),
+        keras.layers.Activation('relu'),
+        keras.layers.Dense(32),
+        keras.layers.Activation('relu'),
+        keras.layers.Dense(1)
+    ])
+    reward_net.compile(optimizer='adam', loss='mse')
+    reward_net.fit(observations_actions, rewards, epochs=15, batch_size=10)
 
     # obs, a => r
     # obs, a => obs
     # obs, a, a, a, ... => r
+    pass
 
 
 if __name__ == "__main__":
